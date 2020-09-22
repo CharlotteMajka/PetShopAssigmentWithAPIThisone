b6eef31abc56a3879920314bacbd043dc408636d
CharlotteMajka <55383161+CharlotteMajka@users.noreply.github.com>
2020-09-22 20:16:10 +0200

    context added

---
+++ PetShop.Infrastructure.SqlData/PetShopAppContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Infrastructure.SqlData
+{
+    public class PetShopAppContext: DbContext
+    {
+        public DbSet<Pet> pets { get; set; }
+
+    }
+}
